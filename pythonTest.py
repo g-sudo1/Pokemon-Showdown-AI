7eeea29838e4425dad7616d2dd84b968499bdf64
GovindButtar <gbuttar@purdue.edu>
2025-10-04 10:51:56 -0400

    Update pythonTest.py

--- pythonTest.py
+++ pythonTest.py
@@ -7,18 +7,39 @@ from poke_env.data import GenData
 import sys
 import random
 from teams import TEAMS, RandomTeamBuilder, team
-from poke_env.ps_client import AccountConfiguration 
+from poke_env.ps_client import AccountConfiguration
+from poke_env.environment import DoubleBattle
+from poke_env.player.battle_order import DoubleBattleOrder
+from poke_env.environment import DoublesEnv
+
 # import mctsAgent
 
 sys.path.append("../src")
+
+
 class YourFirstAgent(Player):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # self.gen_data = GenData.from_gen(9)
-    def choose_move(self, battle):
+    def something(self, i, battle):
+        for move in battle.available_moves[i]:
+                # print("TYPE OF MOVE HERE: ", type(move), "\n")
+            if move.base_power >= 80:
+                    # A powerful move! Let's use it
+                return self.create_order(move)
+                # No available move? Let's switch then!
+        for switch in battle.available_switches[i]:
+            if (
+                switch.current_hp_fraction
+                > battle.active_pokemon[i].current_hp_fraction
+            ):
+                # This other pokemon has more HP left... Let's switch it in?
+                return self.create_order(switch)
+        return (self.choose_random_move(battle))
+    def choose_move_single(self, battle):
         for move in battle.available_moves:
-            print("TYPE OF MOVE HERE: ", [type(k) for k in move], "\n")
-            if move.base_power > 90:
+            print("TYPE OF MOVE HERE: ", type(move), "\n")
+            if move.base_power >= 80:
                 # A powerful move! Let's use it
                 return self.create_order(move)
 
@@ -31,6 +52,21 @@ class YourFirstAgent(Player):
         # Not sure what to do?
         return self.choose_random_move(battle)
 
+    def choose_move(self, battle):
+        if isinstance(battle, DoubleBattle):
+            return DoublesEnv.action_to_order
+        else:
+            return self.choose_move_single(battle)
+
+    def choose_move_double(self, battle):
+        order = []
+        for i in range(2):
+            order.append(self.something(i, battle))
+        return DoubleBattleOrder(*order)
+    async def _handle_ots_request(self, battle_tag: str):
+        pass
+
+
 battle_format = "gen9vgc2025regh"
 team_ids = list(range(len(TEAMS[battle_format[-4:]])))
 # random.Random(0).shuffle(team_ids)
@@ -39,12 +75,24 @@ team_ids = list(range(len(TEAMS[battle_format[-4:]])))
 # print(team1)
 # print(team2)
 # RandomTeamBuilder(team_ids, battle_format)
-firstAgent = YourFirstAgent(account_configuration=AccountConfiguration("wduhwiduhwi", "kyskyskys"), battle_format="gen9balancedhackmons", team = team)
-second_player = RandomPlayer(account_configuration=AccountConfiguration("sduhiduhwidhine", "djiwjdiwjdoijwdojo"), battle_format="gen9balancedhackmons", team = team)
-#for the project we are concerned with gen 9 vgc 2025 reg i format
-#for testing purposes, we are going to use regulation G since someone already has team selection for that
+firstAgent = YourFirstAgent(
+    account_configuration=AccountConfiguration("wduhwiduhwi", "kyskyskys"),
+    battle_format=battle_format,
+    team=team,
+)
+second_player = YourFirstAgent(
+    account_configuration=AccountConfiguration("sduhiduhwidhine", "djiwjdiwjdoijwdojo"),
+    battle_format=battle_format,
+    team=team,
+)
+
+
+# for the project we are concerned with gen 9 vgc 2025 reg i format
+# for testing purposes, we are going to use regulation G since someone already has team selection for that
 async def battle():
     await firstAgent.battle_against(second_player, n_battles=1)
+
+
 asyncio.run(battle())
 print(
     f"Player {firstAgent.username} won {firstAgent.n_won_battles} out of {firstAgent.n_finished_battles} played"
@@ -57,5 +105,3 @@ print(
 
 for battle_tag, battle in firstAgent.battles.items():
     print(battle_tag, battle.won)
-
-
